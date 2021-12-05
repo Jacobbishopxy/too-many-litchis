5c2770afaa7bc5215ce52f1053010b03a2495e8e
Jacob <jacobbishopxy@gmail.com>
2021-12-05 23:17:15 +0800

    108_convert_sorted_array_to_binary_search_tree

---
+++ rs/src/bin/108_convert_sorted_array_to_binary_search_tree.rs
@@ -0,0 +1,26 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use rs::TreeNode;
+
+fn main() {
+    let nums = vec![-10, -3, 0, 5, 9];
+
+    println!("{:?}", sorted_array_to_bst(nums));
+}
+
+fn sorted_array_to_bst(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+    let len = nums.len() as i32;
+    sort(&nums, 0, len - 1)
+}
+
+fn sort(nums: &Vec<i32>, left: i32, right: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    if left > right {
+        return None;
+    }
+    let mid = (left + right) / 2;
+    let mut root = TreeNode::new(nums[mid as usize]);
+    root.left = sort(nums, left, mid - 1);
+    root.right = sort(nums, mid + 1, right);
+    Some(Rc::new(RefCell::new(root)))
+}
